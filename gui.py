7fe1d30e655043fa0afc7aabd890ce20664f89bc
s0mnaths <somnaths027@gmail.com>
2021-01-23 01:36:50 +0530

    Add quality availiblity check

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import PySimpleGUI as sg
 from pytube import YouTube
 from pytube.cli import on_progress
 
-def Download(url, qualityList, path):
+def Download(url, quality, path):
     try:
         video=YouTube(url, on_progress_callback=on_progress)
         streams=video.streams.all()
@@ -16,31 +16,38 @@ def Download(url, qualityList, path):
         print(err)
     else:
         print('Done!')
+
 # Define the window's contents
+qualityList=['1080p','720p','480p','360p','240p','144p']
 layout = [[sg.Text("Enter video URL :")],
-          [sg.Input(key='INPUT')],
+          [sg.Input(key='URL')],
           [sg.Text('Select path to store :')],
           [sg.Input(key='FILEPATH'), sg.FolderBrowse()],
-          [sg.Combo(qualityList, size = (20,4), enable_events=False, key='QUALITY', change_submits=True)],
-          [sg.Text(size=(40,1), key='OUTPUT')],
+          [sg.Combo(qualityList,'Readonly', size = (20,4), enable_events=False, key='QUALITY', change_submits=True)],
+          [sg.Text(size=(40,10), key='OUTPUT')],
           [sg.Button('Download'), sg.Button('Quit')]]
 
-# Create the window
 window = sg.Window('Youtube Downloader', layout)
 
-# Display and interact with the Window using an Event Loop
 while True:
     event, values = window.read()
-    # See if user wants to quit or window was closed
     if event == sg.WINDOW_CLOSED or event == 'Quit':
         break
-    # Output a message to the window
-    #window['OUTPUT'].update('Hello ' + values['INPUT'] + "! Thanks for trying PySimpleGUI") 
-    
-    #update the dropdown value
+    url=values['URL']
     quality = values['QUALITY']
     path=values['FILEPATH']
     window['FILEPATH'].update(path)
 
-# Finish up by removing from the screen
+    video=YouTube(url)
+    streams=video.streams.all()
+    qualityAvail={}
+    for stream in streams:
+            if stream.mime_type=='video/mp4'and stream.is_progressive==False:
+                qualityAvail[stream.resolution.strip()]=stream.itag
+    if quality not in qualityAvail.keys():
+        qualPrint='\n-> '.join([str(k) for k in qualityAvail.keys()])
+        window['OUTPUT'].update('Selected video quality is not available for this video. \nSelect another. Available :\n->'+qualPrint)
+
+
+
 window.close()
